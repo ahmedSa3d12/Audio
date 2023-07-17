24fa77777c167f3a76ddbdd73848a64525164931
TopBussiness <TopBusiness110@gmail.com>
2023-07-17 12:39:16 +0200

    kk

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,6 @@ import 'package:get_it/get_it.dart';
 import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:screen_capture_event/screen_capture_event.dart';
 import 'dart:async';
-
-
-
-
 import 'app.dart';
 import 'app_bloc_observer.dart';
 import 'core/models/user_model.dart';
